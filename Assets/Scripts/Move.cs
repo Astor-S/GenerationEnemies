d1da831321441a6d95af90099f6b9982439a2184
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-07-27 23:01:57 +0300

    Update Move.cs

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,12 +5,12 @@ public class Move : MonoBehaviour
     [SerializeField] private float _speed = 5f;
     private Vector3 _direction;
 
-    void Awake()
+    private void Awake()
     {
         _direction = transform.forward;
     }
 
-    void Update()
+    private void Update()
     {
         transform.Translate(_direction * _speed * Time.deltaTime);
     }
