4660b49e1bf7e201736760b111c6571f141fa898
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-07-29 18:21:59 +0300

    Update Spawner.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField]private Transform[] _spawnPoints;
     [SerializeField] private Enemy _enemyPrefab;
+    
+    [SerializeField]private Transform[] _spawnPoints;
+    [SerializeField] private Vector3[] _spawnDirections;
     [SerializeField] private float _spawnFrequency = 2f;
+    [SerializeField] private int _minRange = 0;
 
     private Coroutine _coroutine;
     private WaitForSeconds _wait;
@@ -26,9 +29,13 @@ public class Spawner : MonoBehaviour
 
         while (isRunning)
         {
-            Transform spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+            int spawnIndex = Random.Range(_minRange, _spawnPoints.Length);
+            Transform spawnPoint = _spawnPoints[spawnIndex];
+            Vector3 spawnDirection = _spawnDirections[spawnIndex];
+
             Enemy newEnemy = Instantiate(_enemyPrefab, spawnPoint.position, Quaternion.identity);
-   
+            newEnemy.SetDirection(spawnDirection);
+
             yield return _wait;
         }
     }
