3d392149a3918ba0803cd807b7644975c2921fab
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-07-30 13:00:48 +0300

    Update Move.cs

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,15 +3,10 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
-    private Vector3 _direction;
-
-    private void Awake()
-    {
-        _direction = transform.forward;
-    }
 
     private void Update()
     {
-        transform.Translate(_direction * _speed * Time.deltaTime);
+        Vector3 direction = transform.forward;
+        transform.Translate(direction * _speed * Time.deltaTime);
     }
 }
